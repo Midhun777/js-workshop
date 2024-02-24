496d876a14cfce9130054b520e75ac9df6da8273
Midhun777 <midmat4828@gmail.com>
2024-02-24 14:46:22 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //otp generation 
 
+var i = 1;
 function generateOtp() {
 	//	import { randomInt } from "crypto";
 	//const otp = randomInt(10000, 1000000);
 	const otp = Math.floor(100000 + Math.random() * 900000);
 	console.log(otp);
 	document.getElementById('otp').innerHTML = otp;
+	document.getElementById('inc').innerHTML = i;
+	i++;
 }
-setInterval(generateOtp, 1000);
+setInterval(generateOtp, 888);
