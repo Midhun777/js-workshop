8d701d204d8379ec6dfe8e16b02f76a21d81dd49
Midhun777 <midmat4828@gmail.com>
2024-02-28 10:11:51 +0530

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 //otp generation 
 
 var i = 1;
+
 function generateOtp() {
-	//	import { randomInt } from "crypto";
-	//const otp = randomInt(10000, 1000000);
+	document.getElementById('otp-result').innerHTML = "Waiting for confirmation..";
 	const otp = Math.floor(100000 + Math.random() * 900000);
-	console.log(otp);
 	document.getElementById('otp').innerHTML = otp;
 	document.getElementById('inc').innerHTML = i;
 	i++;
 }
-setInterval(generateOtp, 888);
+//setInterval(generateOtp, 870);
+function checkOtp() {
+	var otpValue = document.getElementById('otpBox').value;
+	var otp = document.getElementById('otp').innerHTML;
+	if (otpValue == otp) {
+		document.getElementById('otp-result').innerHTML = "Otp Successfully Verified!";
+	} else {
+		document.getElementById('otp-result').innerHTML = "Otp Verification Failed!";
+	}
+}
